e189cd02089ded0857a243dcf6c80ecf21c8e622
Ryan Keane <rwkeane728@gmail.com>
2025-05-26 17:35:45 -0500

    Fix handling pytorch

--- tsercom/rpc/serialization/__init__.py
+++ tsercom/rpc/serialization/__init__.py
@@ -1,11 +1,25 @@
 from tsercom.rpc.serialization.caller_id_extraction import (
-    extract_id_from_call,
-    extract_id_from_first_call,
+    extract_id_from_call,  # noqa: F401
+    extract_id_from_first_call,  # noqa: F401
 )
-from tsercom.rpc.serialization.serializable_tensor import SerializableTensor
 
-__all__ = [
+al = [
     "extract_id_from_call",
     "extract_id_from_first_call",
-    "SerializableTensor",
 ]
+
+import subprocess  # noqa: E402
+
+try:
+    import grpc
+
+    version = grpc.__version__
+    major_minor_version = ".".join(version.split(".")[:2])
+
+    al += [
+        "SerializableTensor",
+    ]
+except (AttributeError, subprocess.CalledProcessError, FileNotFoundError):
+    pass
+
+__all__ = al
